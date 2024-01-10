99b0b002ac055c6d28b32c3bfa8781e8bad8dc23
Alison Pezzott <58135934+alisonpezzott@users.noreply.github.com>
2024-01-10 13:54:40 -0300

    Update get-columns-types-sql-server.sql

--- get-columns-types-sql-server.sql
+++ get-columns-types-sql-server.sql
@@ -1,9 +1,9 @@
 SELECT 
     c.name as column_name
-  , t.name AS data_type,
+  , t.name AS data_type
   , c.max_length
   , c.precision
 FROM sys.columns c
   INNER JOIN sys.types t 
   ON c.user_type_id = t.user_type_id
-WHERE c.object_id = OBJECT_ID('TargetTable')
+WHERE c.object_id = OBJECT_ID('TargetTable');
